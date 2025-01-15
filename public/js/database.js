90dd6d07b53f3a8364914d1307135470f7f60b8b
Brian Lopez <brian_lopez@me.com>
2025-01-15 08:22:12 -0700

    db updates

--- public/js/database.js
+++ public/js/database.js
@@ -2,7 +2,7 @@ export const database = [
   {
     id: 1,
     imageSource: "./assets/rally-bar-huddle.png",
-    baseprice: 12055,
+    baseprice: 1750,
     basehours: {
       installHours: 2,
       configHours: 2,
@@ -25,7 +25,7 @@ export const database = [
       },
       hdmiTxRx: {
         name: "HDMI Tx/Rx",
-        price: 1500,
+        price: 1000,
         optionHours: {
           installHours: 2,
           configHours: 0,
@@ -35,7 +35,7 @@ export const database = [
       },
       hdmiUsbTxRx: {
         name: "HDMI & USB Tx/Rx",
-        price: 3000,
+        price: 2000,
         optionHours: {
           installHours: 4,
           configHours: 0,
@@ -45,54 +45,64 @@ export const database = [
       },
     },
     displays: {
+      OfeDisplay: {
+        name: "Owner Furnished Display",
+        price: 700,
+        optionHours: {
+          installHours: 4,
+          configHours: 2,
+          programingHours: 0,
+          managementHours: 6,
+        },
+      },
       CDE5530: {
         name: "Viewsonic CDE5530",
         price: 1000,
         optionHours: {
-          installHours: 2,
+          installHours: 4,
           configHours: 0,
           programingHours: 0,
-          managementHours: 0,
+          managementHours: 4,
         },
       },
       CDE6530: {
         name: "Viewsonic CDE6530",
-        price: 600,
-        optionHours: {
-          installHours: 2,
-          configHours: 0,
-          programingHours: 0,
-          managementHours: 0,
-        },
-      },
-      CDE7530: {
-        name: "Viewsonic CDE7530",
-        price: 700,
-        optionHours: {
-          installHours: 3,
-          configHours: 0,
-          programingHours: 0,
-          managementHours: 0,
-        },
-      },
-      CDE8630: {
-        name: "Viewsonic CDE8630",
-        price: 1800,
+        price: 1400,
         optionHours: {
           installHours: 4,
           configHours: 0,
           programingHours: 0,
-          managementHours: 0,
+          managementHours: 4,
+        },
+      },
+      CDE7530: {
+        name: "Viewsonic CDE7530",
+        price: 1500,
+        optionHours: {
+          installHours: 4,
+          configHours: 0,
+          programingHours: 0,
+          managementHours: 4,
+        },
+      },
+      CDE8630: {
+        name: "Viewsonic CDE8630",
+        price: 2800,
+        optionHours: {
+          installHours: 5,
+          configHours: 0,
+          programingHours: 0,
+          managementHours: 5,
         },
       },
       CDE9830: {
         name: "Viewsonic CDE9830",
         price: 5000,
         optionHours: {
-          installHours: 5,
+          installHours: 6,
           configHours: 0,
           programingHours: 0,
-          managementHours: 0,
+          managementHours: 6,
         },
       },
     },
@@ -121,12 +131,12 @@ export const database = [
       },
       rallyMic: {
         name: "Rally Mic Pod",
-        price: 800,
+        price: 700,
         optionHours: {
-          installHours: 2,
+          installHours: 3,
           configHours: 0,
           programingHours: 0,
-          managementHours: 0,
+          managementHours: 3,
         },
       },
     },
